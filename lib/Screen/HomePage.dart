59afc535c42ac88e44f533bf0bcf7b6abb3e04d7
Rohit Giri <giri420rohit@gmail.com>
2024-05-18 18:45:23 +0530

    homeScreen ui created

--- lib/Screen/HomePage.dart
+++ lib/Screen/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:email_auth/Screen/Login.dart';
+import 'package:email_auth/Widget/UiHelper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -11,14 +12,28 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descContrller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(actions: [
         IconButton(onPressed: (){LogOut();}, icon: Icon(Icons.logout))
       ],),
-      body: Center(
-        child: Text("User Login Sucessfully"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 8),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Uihelper().CustomTextField(titleController,"Title", false, Icons.title_rounded, (){}),
+              Uihelper().CustomTextField(descContrller, "Description", false, Icons.description, (){}),
+              Uihelper().CustomButton("Save", Colors.orange, (){})
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -28,6 +43,7 @@ class _HomepageState extends State<Homepage> {
       return Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login(),));
     },);
 
+}
 
 }
-}
+
